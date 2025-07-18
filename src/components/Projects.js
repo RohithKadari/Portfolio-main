8e329ccc7ee687a2c8bbc1f98ad3115505530019
RohithKadari <rohithkadari12@gmail.com>
2025-07-18 11:03:34 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,18 +9,6 @@ function Projects() {
       title: "Educational Task Management",
       description: "The Educational Task Management System is a collaborative platform designed to streamline task assignment and tracking between tutors and students. Tutors can create and assign tasks to students, define deadlines, and schedule timelines for completion. Students can update their progress by submitting reports, with the system automatically tracking the status of each task(completed or pending). Tutors can monitor task completion rates, pending tasks, and generate completion percentage statistics. Deadlines are enforced, ensuring students cannot submit tasks after the due date. "
     },
-    {
-      title: "Apple Stock Price Prediction",
-      description: "Developed a machine learning model for predicting Apple stock prices using Linear Regression, leveraging features such as open, close, high, low, and volume. Achieved accurate predictions of the Adj Close variable, showcasing proficiency in financial data analysis and predictive modeling."
-    },
-    {
-      title: "Mental Math using C",
-      description: "I have developed a project named “Mental Math” that offers a user-friendly interface with options for addition, subtraction, multiplication, and division, dynamically generating random arithmetic questions to enhance users’ math skills, acknowledging correct answers and displaying the correct solution for incorrect responses, contributing to a gamified learning experience within the application."
-    },
-    {
-      title: "Literacy Rate Analysis",
-      description: "In this project, I have done research on the Literacy rate in India, gathered information, and visualized those data in the form of graphs and charts. And I have designed a model to predict the future literacy rate in upcoming years. "
-    }
   ];
 
   const handlePrevClick = () => {
